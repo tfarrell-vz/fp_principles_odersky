919bd695e43f55357d4bb000d7d88f95330fdf91
tfarrell-vz <farrell@abiresearch.com>
2015-05-15 11:59:07 -0400

    My shot at the solution Odersky proposed.

---
+++ week1/NewtonsMethod.scala
@@ -0,0 +1,26 @@
+object NewtonsMethod {
+
+  def abs(x: Double): Double = {
+    if (x >= 0) x
+    else -x
+  }
+
+  def isGoodEnough(guess: Double, x: Double) = {
+    if (abs((guess * guess - x)) < 0.01) true
+    else false
+  }
+
+  def improve(guess: Double, x: Double): Double = {
+    val quotient = x / guess
+    val mean = (quotient + guess) / 2
+    return mean
+  }
+
+  def sqrIter(guess:Double, x:Double):Double = {
+    if (isGoodEnough(guess, x)) guess
+    else sqrIter(improve(guess, x), x)
+  }
+  def main(args: Array[String]): Unit = {
+    println("The estimated square root of 5 starting with a guess of 1 is: " + sqrIter(1.0, 5.0))
+  }
+}
