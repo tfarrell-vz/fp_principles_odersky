46ce68446ba0ace4bd82d006df3c635563544e43
tfarrell-vz <farrell@abiresearch.com>
2015-05-15 09:59:55 -0400

    Newton's method with a hardcoded fixed number of recursive iterations.

--- week1/newtons_method.scala
+++ week1/newtons_method.scala
@@ -1,3 +1,16 @@
+object Newton {
+  def sqr_nm(num: Double):Double = {
+    def loop(est: Double, count: Int): Double  = {
+      val quotient = num / est
+      val mean = { (quotient + est) / 2.0 }
+      if (count == 0) mean
+      else {
+        loop(mean, count - 1)
+      }
+    }
+    loop(1.0, 5)
+  }
+}
 
 
 // vim: set ts=2 sw=2 et sts=2:
