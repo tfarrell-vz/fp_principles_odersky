7deb99efe84843d79506d1c125b1da3d61e83b13
tfarrell-vz <farrell@abiresearch.com>
2015-05-18 13:34:14 -0400

    Higher ordered sum function: Provide a function to apply to discrete values between two integers.

---
+++ week2/HoF.scala
@@ -0,0 +1,10 @@
+object HoF {
+  def sum(f: Int => Int)(a: Int, b: Int): Int = {
+    @annotation.tailrec
+    def loop(a: Int, acc: Int): Int = {
+      if (a > b) acc
+      else loop(a+1, f(a)+acc)
+    }
+    loop(a, 0)
+  }
+}
