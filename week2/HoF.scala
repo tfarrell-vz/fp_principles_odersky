710d348082a59f5ce6c24eaeb73214e14cde8dbf
tfarrell-vz <farrell@abiresearch.com>
2015-05-19 10:50:29 -0400

    HoF for product of a function application over an integer interval.

--- week2/HoF.scala
+++ week2/HoF.scala
@@ -7,4 +7,13 @@ object HoF {
     }
     loop(a, 0)
   }
+
+  def product(f: Int => Int)(a: Int, b: Int): Int = {
+    if (a > b) 1
+    else f(a) * product(f)(a+1, b)
+  }
+
+  def cube(x: Int): Int = x * x * x
+  def square(x: Int): Int = x * x
+  def id(x: Int): Int = x
 }
