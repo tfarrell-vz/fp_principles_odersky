a572815c8ae2e463503192eb47b7c16fb3a90371
tfarrell-vz <farrell@abiresearch.com>
2016-03-22 21:02:56 -0400

    Parentheses balance check

---
+++ week1/ParenthesesBalancing.scala
@@ -0,0 +1,25 @@
+object ParenthesesBalancing {
+  /**
+   * Verifies whether the parentheses in a given character list (string) are balanced.
+   * @param chars
+   * @return
+   */
+  def balance(chars: List[Char]): Boolean = {
+    def loop(chars: List[Char], balance: Int): Int = {
+      if (chars.isEmpty) balance
+      else {
+        if (balance < 0) balance
+        else {
+          val head = chars.head
+          val tail = chars.tail
+          if (head == '(') loop(tail, balance+1)
+          else if (head == ')') loop(tail, balance-1)
+          else loop(tail, balance)
+        }
+      }
+    }
+    val balance = loop(chars, 0)
+    if (balance == 0) true
+    else false
+  }
+}
