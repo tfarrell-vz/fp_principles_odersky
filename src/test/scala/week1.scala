1eecdc5704da0843df71e954906d78d004434bd2
tfarrell-vz <farrell@abiresearch.com>
2016-03-24 11:06:49 -0400

    Redefine the tests for ParenthesisBalancing to be more explicit. Include test for PascalsTriangle.

--- src/test/scala/week1.scala
+++ src/test/scala/week1.scala
@@ -3,8 +3,8 @@ import org.scalatest.FunSpec
 import week1.assignments._
 
 class ParenthesisBalancingSpec extends FunSpec {
-  describe("A string, represented as a character list") {
-    describe("when balanced") {
+  describe("A ParenthesisBalancer") {
+    describe("when given a string containing balanced parenthesis") {
       it("should return true") {
         val str0 = "(if (zero? x) max (/ 1 x))".toList
         val str1 = "I told him (that it's not (yet) done). (But he wasn't listening)".toList
@@ -13,7 +13,7 @@ class ParenthesisBalancingSpec extends FunSpec {
         assert(ParenthesesBalancing.balance(str1) == true)
       }
     }
-    describe("when unbalanced") {
+    describe("when given a string containing unbalanced parenthesis") {
       it("should return false") {
         val unbalanced0 = ":-)".toList
         assert(ParenthesesBalancing.balance(unbalanced0) == false)
@@ -23,6 +23,14 @@ class ParenthesisBalancingSpec extends FunSpec {
         val unbalanced1 = "())(".toList
         assert(ParenthesesBalancing.balance(unbalanced1) == false)
       }
+    }
+  }
+}
+
+class PascalsTriangleSpec extends FunSpec {
+  describe("Pascal's triangle") {
+    it("should have value 1 at position(0,2)") {
+      assert(PascalsTriangle.pascal(0,2) == 1)
     }
   }
 }
