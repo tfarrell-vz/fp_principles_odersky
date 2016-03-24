1813c0b8b695f2704b8ba8c05121bda3be0fdc54
tfarrell-vz <farrell@abiresearch.com>
2016-03-24 11:05:40 -0400

    Move PascalsTriangle into main/scala/week1

--- src/main/scala/week1/PascalsTriangle.scala
+++ src/main/scala/week1/PascalsTriangle.scala
@@ -2,8 +2,8 @@ package week1.assignments
 
 object PascalsTriangle {
   def pascal(c: Int, r: Int): Int = {
-    if (c == 1) 1
-    else if (c == r) 1
+    if (c == 0) 1
+    else if (c == 0) 1
     else {
       pascal(c-1, r-1) + pascal(c, r-1)
     }
