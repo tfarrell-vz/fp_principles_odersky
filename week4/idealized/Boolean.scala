c62250d8c2ea965979a8cb501779678b852a16b0
tfarrell-vz <farrell@abiresearch.com>
2016-05-19 15:16:21 -0400

    Implement a Boolean class.

--- week4/idealized/Boolean.scala
+++ week4/idealized/Boolean.scala
@@ -3,14 +3,14 @@ package idealized.scala
 abstract class Boolean1 {
   def ifThenElse[T](t: => T, e: => T): T
 
-  def && (x: => Boolean1): Boolean1 = ifThenElse(x, false)
-  def || (x: => Boolean1): Boolean1 = ifThenElse(true, x)
-  def unary_!: Boolean            = ifThenElse(false, true)
+  def && (x: => Boolean1): Boolean1 = ifThenElse(x, false1)
+  def || (x: => Boolean1): Boolean1 = ifThenElse(true1, x)
+  def unary_!: Boolean            = ifThenElse(false1, true1)
 
   def == (x: Boolean1): Boolean1    = ifThenElse(x, x.unary_!)
   def != (x: Boolean1): Boolean1    = ifThenElse(x.unary_!, x)
 
-  def < (x: Boolean1): Boolean1     = ifThenElse()
+  def < (x: Boolean1): Boolean1     = ifThenElse(false1, x)
 }
 
 object true1 extends Boolean1 {
