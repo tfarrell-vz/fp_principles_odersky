9afc7bdf73b8ed89bea4a548a3f6963e7dca9ca4
tfarrell-vz <farrell@abiresearch.com>
2016-05-19 15:24:53 -0400

    Natural number from first principles

--- week4/idealized/Int.scala
+++
@@ -1,3 +0,0 @@
-package idealized.scala
-
-class IntI
---
+++ week4/idealized/Nat.scala
@@ -0,0 +1,10 @@
+package idealized.scala
+
+abstract class Nat {
+  def isZero: Boolean
+  def predecessor: Nat
+  def successor: Nat
+  def + (that: Nat): Nat
+  def - (that: Nat): Nat
+}
+
