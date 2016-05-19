90951817e51ec038079007f8d49eaee047be96d9
tfarrell-vz <farrell@abiresearch.com>
2016-05-19 15:39:42 -0400

    Implement a Zero for natural numbers

--- week4/idealized/Nat.scala
+++ week4/idealized/Nat.scala
@@ -8,3 +8,15 @@ abstract class Nat {
   def - (that: Nat): Nat
 }
 
+object Zero extends Nat {
+  def isZero: Boolean = true
+  def predecessor: Nat = throw new NoSuchElementException("Negative: Zero is the smallest natural number.")
+  def successor: Nat = Succ(Zero)
+  def + (that: Nat): Nat = that
+  def - (that: Nat): throw new NoSuchElementException("Negative: Zero is the smallest natural number.")
+}
+
+/* Succ represents the natural number that is one bigger than the one given */
+class Succ(n: Nat) extends Nat {
+
+}
